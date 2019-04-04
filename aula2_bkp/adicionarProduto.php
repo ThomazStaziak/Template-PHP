9435c7fd63305290107d038b000dfe8fbf159b3f
ThomazStaziak <thostaziak@gmail.com>
2019-04-03 21:16:07 -0300

    adding the more classes

--- aula2_bkp/adicionarProduto.php
+++ aula2_bkp/adicionarProduto.php
@@ -10,27 +10,6 @@
   <title>Document</title>
 </head>
 <body>
-
-  <!-- variaveis de conteudo -->
-  <?php
-    $nomeCurso1 = "Full Stack";
-    $descricaoCurso1 = "Curso de desenvolvimento web";
-    $imagemCurso1 = "full.jpeg";
-    $precoCurso1 = 49.99;
-
-    $nomeCurso2 = "Marketing Digital";
-    $descricaoCurso2 = "Curso de Marketing digital... não o orgânico";
-    $imagemCurso2 = "marketing.jpg";
-    $precoCurso2 = 71.99;
-
-    $usuario = [
-      "nome" => "Thomaz Staziak",
-      "email" => "thomaz@digitalhouse.com",
-      "senha" => "123456",
-      "nivelAcesso" => mt_rand(0, 1)
-    ]
-   ?>
-
   <main>
     <!-- navBar -->
     <nav class="navbar navbar-default">
@@ -46,7 +25,7 @@
               <li class="dropdown">
                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Ações <span class="caret"></span></a>
                 <ul class="dropdown-menu">
-                  <li><a href="adicionarProduto.php">Adicionar Produto</a></li>
+                  <li><a href="#">Adicionar Produto</a></li>
                   <li><a href="#">Editar Produto</a></li>
                 </ul>
               </li>
@@ -67,39 +46,32 @@
         </div>
       </div>
     </nav>
-
-    <!-- produtos -->
-    <div id="products" class="container">
-      <div class="row">
-        <div class="col-xs-4 col-sm-6 col-md-6 col-lg-6">
-          <div class="thumbnail">
-            <img src="<?php echo "assets/img/$imagemCurso1"; ?>" alt="Full Stack">
-            <div class="caption">
-              <h3><?php echo $nomeCurso1; ?></h3>
-              <p><?php echo $descricaoCurso1; ?></p>
-              <p><strong>R$ <?php echo $precoCurso1; ?> </strong></p>
-              <a href="#" class="btn btn-primary" role="button">Comprar</a>
-            </div>
-          </div>
+    <div class="container">
+      <form class="col-xs-5 col-sm-5 col-md-5 col-lg-5">
+        <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
         </div>
-
-        <div class="col-xs-4 col-sm-6 col-md-6 col-lg-6">
-          <div class="thumbnail">
-            <img src="<?php echo "assets/img/$imagemCurso2"; ?>" alt="Marketing Digital">
-            <div class="caption">
-              <h3><?php echo $nomeCurso2; ?></h3>
-              <p><?php echo $descricaoCurso2; ?></p>
-              <p><strong>R$ <?php echo $precoCurso2; ?> </strong></p>
-              <a href="#" class="btn btn-primary" role="button">Comprar</a>
-            </div>
-          </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
         </div>
-      </div>
+        <div class="form-group">
+          <label for="exampleInputFile">File input</label>
+          <input type="file" id="exampleInputFile">
+          <p class="help-block">Example block-level help text here.</p>
+        </div>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox"> Check me out
+          </label>
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
     </div>
   </main>
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" charset="utf-8"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-  <script src="assets/js/main.js" charset="utf-8"></script>
 </body>
 </html>
--- aula4/index.php
+++ aula4/index.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-  <link rel="stylesheet" href="assets/css/style.css">
-  <title>Document</title>
-</head>
-<body>
-
-  <?php
+<?php
     $cursos = [
       "Full Stack" => ["full.jpeg", "Curso de desenvolvimento web", 41.99, "fullstack"],
       "Marketing Digital" => ["marketing.jpg", "Curso de marketing", 51.99, "marketing"],
@@ -23,105 +10,156 @@
       "nome" => "Thomaz Staziak",
       "email" => "thomaz@digitalhouse.com",
       "senha" => "123456",
-      "nivelAcesso" => 1
-    ]
-   ?>
+      "nivelAcesso" => mt_rand(0, 1)
+    ];
+  ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+        integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+
+  <header>
+    <!-- navBar -->
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">
+                    <span>Cursos</span>
+                </a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <?php if ($usuario["nivelAcesso"] == 1): ?>
+                  <ul class="nav navbar-nav">
+                      <li class="dropdown">
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                              aria-haspopup="true" aria-expanded="false">Ações <span class="caret"></span></a>
+                          <ul class="dropdown-menu">
+                              <li><a href="#">Adicionar Produto</a></li>
+                              <li><a href="#">Editar Produto</a></li>
+                          </ul>
+                      </li>
+                  </ul>
+                <?php endif; ?>
+                <form id="formulario" class="navbar-form navbar-left">
+                    <div class="form-group">
+                        <input id="barraBusca" type="text" class="form-control" placeholder="Pesquisar...">
+                    </div>
+                    <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"
+                            aria-hidden="true"></span></button>
+                </form>
+                <p class="navbar-text navbar-right">
+                    Logado como
+                    <strong>
+                        <a href="#" class="navbar-link">
+                            <?php echo $usuario["nome"] ?></a>
+                    </strong>
+                </p>
+            </div>
+        </div>
+    </nav>
+  </header>
 
   <main>
-    <?php if ($usuario["nivelAcesso"] == 0) : ?>
-      <nav class="navbar navbar-default">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <a class="navbar-brand" href="#">
-              <span>Cursos</span>
-            </a>
-          </div>
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <form class="navbar-form navbar-left" role="search">
-              <div class="form-group">
-                <input type="text" class="form-control" placeholder="Pesquisar...">
-              </div>
-              <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
-            </form>
-
-            <p class="navbar-text navbar-right">Logado como <a href="#" class="navbar-link"><?php echo $usuario["nome"] ?></a></p>
-          </div>
-        </div>
-      </nav>
-    <?php else : ?>
-      <nav class="navbar navbar-default">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <a class="navbar-brand" href="#">
-              <span>Cursos</span>
-            </a>
-
-          </div>
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Ações <span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                  <li><a href="#">Adicionar Produto</a></li>
-                  <li><a href="#">Editar Produto</a></li>
-                </ul>
-              </li>
-            </ul>
-            <form class="navbar-form navbar-left">
-              <div class="form-group">
-                <input type="text" class="form-control" placeholder="Search">
-              </div>
-              <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
-            </form>
-            <p class="navbar-text navbar-right">Logado como <a href="#" class="navbar-link"><?php echo $usuario["nome"] ?></a></p>
-          </div>
-        </div>
-      </nav>
-    <?php endif; ?>
-
-    <div class="container">
-      <div class="row">
-        <?php foreach ($cursos as $nome => $infos) : ?>
-            <div class="col-xs-4 col-sm-6 col-md-6 col-lg-6">
-              <div class="thumbnail">
-                <img src="<?php echo "assets/img/".$infos[0] ?>" alt="<?php echo "Foto " . $cursos["nome"][$i]; ?>">
-                <div class="caption">
-                  <h3><?php echo $nome  ?></h3>
-                  <p><?php echo $infos[1]  ?></p>
-                  <p><strong>R$ <?php echo $infos[2]  ?> </strong></p>
-                  <a href="#" class="btn btn-info btn-lg" data-toggle="modal" data-target="<?php echo "#$infos[3]" ?> ">Comprar</a>
+      <div class="container">
+          <div class="row">
+              <!-- produtos -->
+              <?php foreach ($cursos as $nome => $infos) : ?>
+                <div class="col-xs-4 col-sm-6 col-md-6 col-lg-6">
+                    <div class="thumbnail">
+                        <img src="<?php echo " assets/img/".$infos[0] ?>" alt="<?php echo "Foto " . $cursos["nome"][$i]; ?>">
+                        <div class="caption">
+                            <h3>
+                                <?php echo $nome  ?>
+                            </h3>
+                            <p>
+                                <?php echo $infos[1]  ?>
+                            </p>
+                            <p><strong>R$
+                                    <?php echo $infos[2]  ?> </strong></p>
+                            <a href="#" class="btn btn-info btn-lg" data-toggle="modal" data-target="<?php echo " #$infos[3]" ?>">Comprar</a>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-          <?php endforeach; ?>
-      </div>
-    </div>
+              <?php endforeach; ?>
+              <!-- área de compra  -->
+              <?php foreach ($cursos as $nome => $infos): ?>
+                <div class="modal fade" id="<?php echo $infos[3] ?>" role="dialog">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                <h4 class="modal-title">Digite seus dados para efetuar a compra</h4>
+                            </div>
+                            <div class="modal-body">
+                                <h4>Curso de
+                                    <?php echo $nome ?>.</h4>
+                                <h4>Valor: R$
+                                    <?php echo $infos[2] ?>
+                                </h4>
+                                <form action="validarCompra.php" method="post">
+                                    <input type="hidden" name="nomeCurso" value="<?php echo $nome ?>">
+                                    <input type="hidden" name="preco" value="<?php echo $infos[2] ?>">
+                                    <div class="input-group col-md-5">
+                                        <label for="labelNome">Nome Completo</label>
+                                        <input id="labelNome" type="text" name="nomeCompleto" class="form-control" placeholder="Digite Aqui...">
+                                    </div>
 
-    <?php foreach ($cursos as $nome => $infos): ?>
-      <div class="modal fade" id="<?php echo $infos[3] ?>" role="dialog">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal">&times;</button>
-              <h4 class="modal-title">Digite seus dados para efetuar a compra</h4>
-            </div>
-            <div class="modal-body">
-              <p>Curso de <?php echo $nome ?>.</p>
-              <p>Valor: R$ <?php echo $infos[2] ?></p>
-              <form class="" action="" method="">
-                <input type="text" name="" value="">
-                <input type="text" name="" value="">
-              </form>
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            </div>
+                                    <div class="input-group col-md-5">
+                                        <label for="labelCpf">CPF</label>
+                                        <input id="labelCpf" type="number" name="cpf" class="form-control" placeholder="Digite Aqui...">
+                                    </div>
+
+                                    <div class="input-group col-md-5">
+                                        <label for="labelNroCartao">Número do Cartão</label>
+                                        <input id="labelNroCartao" type="number" name="nroCartao" class="form-control" placeholder="Digite Aqui...">
+                                    </div>
+
+                                    <div class="input-group col-md-5">
+                                        <label for="labelValidade">Validade</label>
+                                        <input id="labelValidade" type="month" name="validade" class="form-control" placeholder="Digite Aqui...">
+                                    </div>
+
+                                    <div class="input-group col-md-5">
+                                        <label for="labelCvv">CVV</label>
+                                        <input id="labelCvv" type="number" name="cvv" class="form-control" placeholder="Digite Aqui...">
+                                    </div>
+
+                                    <button type="submit" class="btn btn-info">Comprar</button>
+                                </form>
+                            </div>
+                            <div class="modal-footer">
+                                <p align="center">Copyright &copy; Cursos 2019</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              <?php endforeach; ?>
           </div>
-        </div>
       </div>
-    <?php endforeach; ?>
   </main>
+
+  <footer class="footer">
+      <div class="container">
+          <p class="text-muted">Place sticky footer content here.</p>
+      </div>
+  </footer>
+
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" charset="utf-8"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <script src="assets/js/main.js" charset="utf-8"></script>
 </body>
+
 </html>
